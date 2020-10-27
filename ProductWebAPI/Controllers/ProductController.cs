27f4d5b2546d83f11d9071dc96d5b774ce864536
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:53:07 -0500

    Add project files.

---
+++ ProductWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebAPI.Model;
+
+namespace ProductWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        // GET: api/Product
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.Name = " Sample ";
+            product.Category = " Stuff ";
+            product.Price = 2.99M;
+
+            return new string[] { product.Id.ToString(), product.Name, product.Category, product.Price.ToString() };
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
